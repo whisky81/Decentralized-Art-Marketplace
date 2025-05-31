105864e17e96540554f81893bc5769fb65f37f7f
whisky81 <anh666666anh@gmail.com>
2025-05-31 23:30:25 +0700

    fix: compile from local blockchain network

--- src/hardhat.config.js
+++ src/hardhat.config.js
@@ -1,16 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomicfoundation/hardhat-ignition-ethers");
 const { vars } = require("hardhat/config");
-const SEPOLIA_HTTP_PROVIDER = vars.get("SEPOLIA_HTTP_PROVIDER")
-const SEPOLIA_PK = vars.get("SEPOLIA_PK")
+// const SEPOLIA_HTTP_PROVIDER = vars.get("SEPOLIA_HTTP_PROVIDER")
+// const SEPOLIA_PK = vars.get("SEPOLIA_PK")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
-  networks: {
-    sepolia: {
-      url: SEPOLIA_HTTP_PROVIDER,
-      accounts: [SEPOLIA_PK]
-    }
-  }
+  // networks: {
+  //   sepolia: {
+  //     url: SEPOLIA_HTTP_PROVIDER,
+  //     accounts: [SEPOLIA_PK]
+  //   }
+  // }
 };
